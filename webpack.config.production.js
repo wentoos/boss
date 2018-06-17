2f76019d34401f16d34717a344923bb4a0e7cfc7
wentoo <yuwt@shinetechchina.com>
2018-06-17 21:55:55 +0800

    ok

---
+++ webpack.config.production.js
@@ -0,0 +1,142 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const isDebug = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  mode: isDebug ? 'development' : 'production',
+  entry: {
+    vendor: [
+      "react",
+      "mobx",
+      "mobx-react",
+      "react-dom",
+      "react-router",
+      "whatwg-fetch"
+    ],
+    app: ["./src/index"]
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "assets/[name].[hash].js",
+    chunkFilename: "assets/[name].[chunkhash].js"
+  },
+  devtool: 'source-map',
+  optimization: {
+    runtimeChunk: {
+      name: 'manifest'
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // set to true if you want JS source maps,
+        uglifyOptions: {
+          warnings: false
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ],
+    splitChunks:{
+      chunks: 'async',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      name: false,
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          chunks: 'initial',
+          priority: -10,
+          reuseExistingChunk: false,
+          test: /node_modules\/(.*)\.js/
+        },
+        styles: {
+          name: 'styles',
+          test: /\.(scss|css)$/,
+          chunks: 'all',
+          minChunks: 1,
+          reuseExistingChunk: true,
+          enforce: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, "src"),
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss|css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader?sourceMap",
+          "resolve-url-loader",
+          "sass-loader?sourceMap"
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          "file-loader?hash=sha512&digest=hex&name=[hash].[ext]",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              optipng: {
+                optimizationLevel: 7
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+              mozjpeg: {
+                quality: 65,
+                progressive: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new MiniCssExtractPlugin({
+      filename: 'css/app.[name].css',
+      chunkFilename: 'css/app.[contenthash:12].css'
+    }),
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: "./index.html"
+    })
+  ]
+};
